638ed0836f691ec01846091613d5bc97eae8a5b6
Matheus Duarte Benedito <matheusduartte@hotmail.com>
2026-02-15 15:55:35 -0300

    ajuste

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -10,7 +10,7 @@ export default function Login() {
     // Login State
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [rememberMe, setRememberMe] = useState(true);
+
 
     // SignUp State
     const [newName, setNewName] = useState('');
