e7c165e32e14a5c4ba1eab53815e00c7ec4ab485
Matheus Duarte Benedito <matheusduartte@hotmail.com>
2026-02-16 15:20:15 -0300

    ajuste light mode

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { useExpenses } from '../context/ExpenseContext';
 // import { useToast } from '../hooks/useToast';
 import { useAuth } from '../context/AuthContext';
-import { useTheme } from '../context/ThemeContext';
+
 import { Home, DollarSign, Settings, Wallet, PieChart, Map, LogOut, User } from 'lucide-react';
 import { useState } from 'react';
 import CreateTripModal from './CreateTripModal';
